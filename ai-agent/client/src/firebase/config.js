056d00f97a213d2cea4b52804e5e718fc9fd4f0b
shinhajun <martinhajuna@gmail.com>
2025-05-21 12:36:12 +0900

    Firebase API 키 수정

--- ai-agent/client/src/firebase/config.js
+++ ai-agent/client/src/firebase/config.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // Replace with your Firebase configuration from the Firebase console
 const firebaseConfig = {
-  apiKey: "AIzaSyBBk_eoW4B7KwQ5W5fpEHnIfMf1GaRVY8Y",
+  apiKey: "AIzaSyA6JdRUKcOethZDlxMDgrRIiUQWzbfDE7s",
   authDomain: "ai-agent-cad35.firebaseapp.com",
   projectId: "ai-agent-cad35",
   storageBucket: "ai-agent-cad35.appspot.com",
